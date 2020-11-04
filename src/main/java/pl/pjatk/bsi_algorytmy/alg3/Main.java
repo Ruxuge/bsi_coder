59a668b50443c26bc041c017bd1d808476b15161
oskar117 <olek.konieczny@wp.pl>
2020-11-04 18:59:45 +0100

    alg 3 basic solution

--- src/main/java/pl/pjatk/bsi_algorytmy/alg3/Main.java
+++ src/main/java/pl/pjatk/bsi_algorytmy/alg3/Main.java
@@ -1,7 +1,13 @@
 package pl.pjatk.bsi_algorytmy.alg3;
+/*
+    exercise 4
+    https://pejot.sharepoint.com/sites/2021D_GIs_BIS-501c/Shared%20Documents/Forms/AllItems.aspx?id=%2Fsites%2F2021D%5FGIs%5FBIS%2D501c%2FShared%20Documents%2FGeneral%2Freliability%2Epdf&parent=%2Fsites%2F2021D%5FGIs%5FBIS%2D501c%2FShared%20Documents%2FGeneral&p=true&originalPath=aHR0cHM6Ly9wZWpvdC5zaGFyZXBvaW50LmNvbS86Yjovcy8yMDIxRF9HSXNfQklTLTUwMWMvRVRUVllkN0FfbUJKdEd5bFRQMkhkVXdCV3hFUldPdVZrZ252SXVJMkFzcDREQT9ydGltZT12UzZMa2VLQTJFZw
+*/
 
 class Main {
     public static void main(String[] args) {
-        System.out.println("hello");
+        int hours = 5000;
+        float percentage = 0.98f;
+        System.out.format("%.8f",Math.log(percentage)/hours * -1);
     }
 }
