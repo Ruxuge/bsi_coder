7df57e3767c1b96e29e31c51d2c526fcc3e5ce54
oskar117 <olek.konieczny@wp.pl>
2020-11-04 18:38:53 +0100

    Init source

---
+++ src/main/java/pl/pjatk/bsi_algorytmy/alg1/Main.java
@@ -0,0 +1,7 @@
+package pl.pjatk.bsi_algorytmy.alg1;
+
+class Main {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
---
+++ src/main/java/pl/pjatk/bsi_algorytmy/alg2/Main.java
@@ -0,0 +1,7 @@
+package pl.pjatk.bsi_algorytmy.alg2;
+
+class Main {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
