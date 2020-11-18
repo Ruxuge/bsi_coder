4310d4459a89240741337a1f864bdd5215ea84e5
oskar117 <olek.konieczny@wp.pl>
2020-11-18 18:24:31 +0100

    Basic alg 5 solution

--- src/main/java/pl/pjatk/bsi_algorytmy/alg5/Main.java
+++ src/main/java/pl/pjatk/bsi_algorytmy/alg5/Main.java
@@ -1,10 +1,20 @@
 package pl.pjatk.bsi_algorytmy.alg5;
 
+import java.util.*;
+
 /**
  * @author Aleksander Konieczny
  */
 class Main {
     public static void main(String[] args) {
-        System.out.println("hello");
+        List<Row> rows = new ArrayList<>();
+        int costPerBreakdown = 10;
+        rows.add(new Row(0, 3));
+        rows.add(new Row(1, 2));
+        rows.add(new Row(2, 2));
+        rows.add(new Row(3, 3));
+
+        double result = rows.stream().map(e -> e.getNumberOfBreakdowns() * e.getDailyFrequency()).reduce(0.0, (a, b) -> a+b);
+        System.out.println("$"+result);
     }
 }
---
+++ src/main/java/pl/pjatk/bsi_algorytmy/alg5/Row.java
@@ -0,0 +1,38 @@
+package pl.pjatk.bsi_algorytmy.alg5;
+
+public class Row {
+
+    private double numberOfBreakdowns;
+    private double dailyFrequency;
+    private double probability;
+
+    public Row(double numberOfBreakdowns, double dailyFrequency) {
+        this.numberOfBreakdowns = numberOfBreakdowns;
+        this.dailyFrequency = dailyFrequency;
+        this.probability = dailyFrequency / 10;
+    }
+
+    public double getNumberOfBreakdowns() {
+        return numberOfBreakdowns;
+    }
+
+    public void setNumberOfBreakdowns(double numberOfBreakdowns) {
+        this.numberOfBreakdowns = numberOfBreakdowns;
+    }
+
+    public double getDailyFrequency() {
+        return dailyFrequency;
+    }
+
+    public void setDailyFrequency(double dailyFrequency) {
+        this.dailyFrequency = dailyFrequency;
+    }
+
+    public double getProbability() {
+        return probability;
+    }
+
+    public void setProbability(double probability) {
+        this.probability = probability;
+    }
+}
