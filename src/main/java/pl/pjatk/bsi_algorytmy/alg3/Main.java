39058b80cf78f3ff0a0b8f2e7c98675e87313183
oskar117 <olek.konieczny@wp.pl>
2020-11-18 17:49:13 +0100

    Add author tak

--- src/main/java/pl/pjatk/bsi_algorytmy/alg3/Main.java
+++ src/main/java/pl/pjatk/bsi_algorytmy/alg3/Main.java
@@ -3,6 +3,7 @@ package pl.pjatk.bsi_algorytmy.alg3;
 /**
  * What is the highest failure rate for a product if it is to have a reliability (or probability of survival) of 98 percent at 5000 hours?
  * Assume that the time to failure follows an exponential distribution.
+ * @author Aleksander Konieczny
  */
 class Main {
     public static void main(String[] args) {
